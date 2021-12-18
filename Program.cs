91ab38504e2509149d863ba8c954183cd2d603cf
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2021-12-18 14:12:42 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LINQ_ElementAt_ElementOrDefault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Fruits = new List<string>
+            {
+                "Banana", "Peer", "watermelon", "Pomme", "Orange", "Papoy"
+            };
+
+            var Element = Fruits.ElementAt(2);
+
+            Console.WriteLine(Element);
+        }
+    }
+}
